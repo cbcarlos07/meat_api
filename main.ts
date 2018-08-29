04087135ec3e1c488c77dda9c593abc55c220f86
Carlos <cbcarlos08@gmail.com>
2018-08-29 07:17:53 -0400

    Seção 3 Aula 20 Obtendo um único recurso

--- main.ts
+++ main.ts
@@ -1,34 +1,12 @@
-import * as restify from 'restify'
-const server = restify.createServer({
-    name: 'meat_api',
-    version: '1.0.0'
+import { Server } from './server/server'
+import {usersRouter} from "./users/users.router";
+
+const server = new Server()
+server.bootstrap([usersRouter]).then( server=> {
+  console.log( 'Server is listeniing on:', server.application.address() )
+}).catch(error =>{
+    console.log( 'Server failed to start' )
+    console.error(error)
+    process.exit(1)
 })
-server.use(restify.plugins.queryParser())
-server.get('/info', [(req, resp, next)=>{
-    if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
-        // resp.status(400)
-        // resp.json({message: 'Please, update your browser'})
-        let error: any = new Error()
-        error.statusCode = 400
-        error.message = 'Please, update your browser'
-        return next(error)
-    }
-    return next()
-},(req, resp, next)=>{
-//    resp.contentType = 'application/json';
-//    resp.status(400)
-//    resp.setHeader('Content-Type', 'application/json');
-//    resp.send({message: 'hello'})
-//    resp.json(({message: 'hello'}))
-      resp.json({
-          browser: req.userAgent(),
-          method: req.method,
-          url: req.href(),
-          path: req.path(),
-          query: req.query
-      })
-    return next()
-}])
-server.listen(3000, ()=>{
-    console.log( 'API is running on http://localhost:3000' )
-})
+
