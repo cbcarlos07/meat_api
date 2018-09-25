edd63275af6f699cf9e9dd50fde8b400308380e2
Carlos <cbcarlos08@gmail.com>
2018-09-25 13:02:33 -0400

    Seção 7 Aula 40 Router de Restaurantes - restify

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,9 @@
 exports.__esModule = true;
 var server_1 = require("./server/server");
 var users_router_1 = require("./users/users.router");
+var restaurants_router_1 = require("./restaurants/restaurants.router");
 var server = new server_1.Server();
-server.bootstrap([users_router_1.usersRouter]).then(function (server) {
+server.bootstrap([users_router_1.usersRouter, restaurants_router_1.restaurantsRouter]).then(function (server) {
     console.log('Server is listening on:', server.application.address());
 })["catch"](function (error) {
     console.log('Server failed to start');
---
+++ dist/restaurants/restaurants.router.js
@@ -0,0 +1,61 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+var model_router_1 = require("../common/model.router");
+var restify_errors_1 = require("restify-errors");
+var restaurants_model_1 = require("./restaurants.model");
+var RestaurantsRouter = /** @class */ (function (_super) {
+    __extends(RestaurantsRouter, _super);
+    function RestaurantsRouter() {
+        var _this = _super.call(this, restaurants_model_1.Restaurant) || this;
+        _this.findMenu = function (req, resp, next) {
+            restaurants_model_1.Restaurant.findById(req.params.id, "+menu")
+                .then(function (rest) {
+                if (!rest) {
+                    throw new restify_errors_1.NotFoundError('Restaurant not found');
+                }
+                else {
+                    resp.json(rest.menu);
+                    return next();
+                }
+            })["catch"](next);
+        };
+        _this.replaceMenu = function (req, resp, next) {
+            restaurants_model_1.Restaurant.findById(req.params.id)
+                .then(function (rest) {
+                if (!rest) {
+                    throw new restify_errors_1.NotFoundError('Restaurant no found');
+                }
+                else {
+                    rest.menu = req.body; //array de MenuItem
+                    return rest.save();
+                }
+            }).then(function (rest) {
+                resp.json(rest.menu);
+                return next();
+            })["catch"](next);
+        };
+        return _this;
+    }
+    RestaurantsRouter.prototype.appyRoutes = function (app) {
+        app.get('/restaurants', this.findAll);
+        app.get('/restaurants/:id', [this.validateId, this.findById]);
+        app.post('/restaurants', this.save);
+        app.put('/restaurants/:id', [this.validateId, this.replace]);
+        app.patch('/restaurants/:id', [this.validateId, this.update]);
+        app.del('/restaurants/:id', [this.validateId, this["delete"]]);
+        app.get('/restaurants/:id/menu', [this.validateId, this.findMenu]);
+        app.put('/restaurants/:id/menu', [this.validateId, this.replaceMenu]);
+    };
+    return RestaurantsRouter;
+}(model_router_1.ModelRouter));
+exports.restaurantsRouter = new RestaurantsRouter;
