f880263c8e24ebe1dea972575bfe9debda2b0316
Carlos Bruno <cbcarlos08@gmail.com>
2018-10-22 12:29:06 -0400

    Seção 11 Aula 62 Logging com restify e pm2

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,7 @@ import * as mongoose from 'mongoose'
 import { mergePatchBodyParser } from './merge-patch.parser'
 import {handleError} from './error.handler';
 import {tokenParser} from '../security/token.parser'
+import { logger } from '../common/logger';
 export class Server{
     application: restify.Server
     initializeDb(): mongoose.MongooseThenable{
@@ -20,12 +21,16 @@ export class Server{
                 const options: restify.ServerOptions = {
                     name: 'meat-api',
                     version: '1.0.0',
+                    log: logger
                 }
                 if(environment.security.enableHTTPS){
                     options.certificate = fs.readFileSync(environment.security.certificate),
                     options.key = fs.readFileSync(environment.security.key)
                 }
                 this.application = restify.createServer(options)    
+                this.application.pre(restify.plugins.requestLogger({
+                    log: logger
+                }))
                 this.application.use(restify.plugins.queryParser())
                 this.application.use(restify.plugins.bodyParser())
                 this.application.use(mergePatchBodyParser)
@@ -40,6 +45,18 @@ export class Server{
                 })
 
                 this.application.on('restifyError', handleError )
+                /*this.application.on('after', restify.plugins.auditLogger({
+                    log: logger,
+                    event: 'after',
+                    body: true,
+                    server: this.application
+                }))
+                */
+                /*
+                  this.application.on('audit', data=> {
+
+                  })
+                */
 
             }catch(error){
                 reject(error)
